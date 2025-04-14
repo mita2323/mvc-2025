0dd138e0ee8d657f9c9fac3a5a17b3f7bf1993a2
Michelle Tammi <michelle.tammi29@gmail.com>
2025-04-14 19:21:40 +0200

    kmom02 edited api code

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -18,90 +18,105 @@ class ApiController extends AbstractController
             [
                 'namn' => 'Home',
                 'route' => '/',
+                'link' => 'home',
                 'metod' => 'GET',
                 'beskrivning' => 'Hemsidan med information om mig'
             ],
             [
                 'namn' => 'About',
                 'route' => '/about',
+                'link' => 'about',
                 'metod' => 'GET',
                 'beskrivning' => 'About sidan med information om kursen'
             ],
             [
                 'namn' => 'Report',
                 'route' => '/report',
+                'link' => 'report',
                 'metod' => 'GET',
                 'beskrivning' => 'Report sidan med alla redovisningstexter'
             ],
             [
                 'namn' => 'Lucky Number',
                 'route' => '/lucky',
+                'link' => 'lucky',
                 'metod' => 'GET',
                 'beskrivning' => 'Få ett "Lucky number" sidan'
             ],
             [
                 'namn' => 'Landingssida',
                 'route' => '/card',
+                'link' => 'card',
                 'metod' => 'GET',
                 'beskrivning' => 'Landningssida som visar samtliga undersidor och innehåller information om uppgiften.'
             ],
             [
                 'namn' => 'Visa kortlek',
                 'route' => '/card/deck',
+                'link' => 'card_deck',
                 'metod' => 'GET',
                 'beskrivning' => 'Returnerar hela kortleken sorterad efter färg och värde.'
             ],
             [
                 'namn' => 'Blanda kortlek',
                 'route' => '/card/deck/shuffle',
+                'link' => 'deck_shuffle',
                 'metod' => 'GET',
                 'beskrivning' => 'Blandar kortleken.'
             ],
             [
                 'namn' => 'Dra ett kort',
                 'route' => '/card/deck/draw',
+                'link' => 'deck_draw',
                 'metod' => 'GET',
                 'beskrivning' => 'Drar ett kort från kortleken.'
             ],
             [
                 'namn' => 'Dra flera kort',
                 'route' => '/card/deck/draw/number',
+                'link' => 'deck_draw_form',
                 'metod' => 'GET',
                 'beskrivning' => 'Drar ett angivet antal kort från kortleken.'
             ],
             [
                 'namn' => 'JSON kortlek sorterad',
                 'route' => '/api/deck',
+                'link' => 'api_deck',
                 'metod' => 'GET',
                 'beskrivning' => 'Returnerar en JSON struktur med hela kortleken sorterad efter färg och värde.'
             ],
             [
                 'namn' => 'JSON kortlek blandat',
                 'route' => '/api/deck/shuffle',
+                'link' => 'api_deck_shuffle',
                 'metod' => 'POST',
                 'beskrivning' => 'Blandar kortleken och returnerar en JSON-struktur.'
             ],
             [
                 'namn' => 'JSON drar ett kort',
                 'route' => '/api/deck/draw',
+                'link' => 'api_deck_draw',
                 'metod' => 'POST',
                 'beskrivning' => 'Drar ett kort från kortleken och returnerar en JSON-struktur.'
             ],
             [
                 'namn' => 'JSON drar :number kort',
                 'route' => '/api/deck/draw/number',
+                'link' => 'api_deck_draw_number_form',
                 'metod' => 'GET',
                 'beskrivning' => 'Drar ett angivet antal kort från kortleken och returnerar en JSON-struktur med de dragna korten.'
             ],
             [
                 'namn' => 'Visa session',
                 'route' => '/session',
+                'link' => 'session_show',
                 'metod' => 'GET',
                 'beskrivning' => 'Skriver ut innehållet i sessionen.'
             ],
             [
                 'namn' => 'Radera session',
                 'route' => '/session/delete',
+                'link' => 'session_delete',
                 'metod' => 'GET',
                 'beskrivning' => 'Raderar innehåller i sessionen.'
             ],
