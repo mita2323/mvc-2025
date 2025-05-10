ce48286a68f91830ae83d6781fc1188eb3b760bb
Michelle Tammi <michelle.tammi29@gmail.com>
2025-05-10 19:27:35 +0200

    Added create function

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -19,4 +19,40 @@ final class BookController extends AbstractController
             'controller_name' => 'BookController',
         ]);
     }
+
+    #[Route('/library/create', name: 'book_create')]
+    public function createBook(
+        Request $request, ManagerRegistry $doctrine
+    ): Response {
+        if ($request->isMethod('POST')) {
+            $book = new Book();
+            $book->setTitle($request->request->get('title'));
+            $book->setIsbn($request->request->get('isbn'));
+            $book->setAuthor($request->request->get('author'));
+
+            if ($imageFile = $request->files->get('image')) {
+                $newFilename = uniqid('book_', true) . '.' . $imageFile->guessExtension();
+                $imageFile->move($this->getParameter('kernel.project_dir') . '/assets/images', $newFilename);
+                $book->setImage($newFilename);
+            }
+
+            if ($book->getTitle() && $book->getIsbn() && $book->getAuthor()) {
+                $entityManager = $doctrine->getManager();
+                $entityManager->persist($book);
+                $entityManager->flush();
+                return $this->redirectToRoute('book_show_all');
+            }
+
+            $this->addFlash('error', 'All fields except image are required.');
+        }
+
+        return $this->render('book/create.html.twig');
+    }
+
+    #[Route('/library/show', name: 'book_show_all')]
+    public function showAllBooks(
+        BookRepository $bookRepository
+    ): Response {
+        
+    }
 }
